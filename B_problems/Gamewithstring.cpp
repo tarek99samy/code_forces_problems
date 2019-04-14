011ef4a0120e0bd62281dcb48a39a23625cce337
abdalla mahmoud <abdallaxpmostafa@gmail.com>
2019-04-14 18:03:18 +0200

    first steps in solving B & C

---
+++ B_problems/Gamewithstring.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+using namespace std;
+//#include<windows.h>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<cmath>
+#include<list>
+#include<cstdlib>
+#include<memory>
+long long GCD(long long A, long long B) 
+{
+	if (B > A) 
+	{
+		swap(A, B); 
+	}
+	long long gcd = A % B;
+	if (gcd == 0) 
+	{
+		return B;
+	}
+	long long next;
+	while (gcd!=0)
+	{
+		next = gcd;
+		A=B;
+		B = gcd;
+		gcd = A % B;
+
+	}
+	return next;
+
+}
+long long fact(long long num) 
+{
+	long long factorial = 1;
+	for (long long i  = 1; i <=num; i++)
+		factorial *= i;
+	return factorial;
+}
+long long C(long long r, long long n) 
+{
+	
+	long long num = 1; 
+	for (long long i = r; i > r-n; i--) 
+	{
+
+		num = num * i;
+	
+	}
+	return num / fact(n);
+}
+int min_three(int n1, int n2, int n3) 
+{
+	int _min = n1;
+	if (n1 < min(n2, n3))
+		return n1;
+	else 
+		return min(n2, n3);
+	
+
+}
+int Sum_of_digits(long long num)
+{
+	int sum = 0;
+	while (num != 0)
+	{
+		sum += num % 10;
+		num /= 10;
+	}
+	return num;
+}
+int main()
+{
+	string  s1;
+	cin >> s1;
+	int i = 0,turn=0;
+	int size = s1.size();
+	while (i<size-1) 
+	{
+		if (s1[i] == s1[i + 1]) 
+		{
+			s1.erase(s1.begin() + i);
+			s1.erase(s1.begin() + i);
+			i-=2;
+			size -= 2;
+			turn++;
+		}
+
+		i++;
+		if (i < 0)
+			i = 0;
+	}
+	if (turn % 2 == 0) 
+		cout << "NO\n ";
+	else
+		cout << "YES\n ";
+	
+	return 0;
+}
